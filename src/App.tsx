f1930927d54df634b39299c90b48f21c79e257aa
stanislavberger <berger.pro@yandex.ru>
2024-06-07 16:02:32 +0300

    Inital commit: Front

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import LoginForm from './components/LoginForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,30 @@
+import '../App.css'
+import React, { FC, useContext, useState } from "react";
+import { Context } from "../index";
+
+const LoginForm: FC = () => {
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const {store} = useContext(Context)
+
+    return(
+        <div className="">
+            <input 
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                type="text"
+                placeholder="Email"
+            />
+            <input 
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                type="password"
+                placeholder="Введите пароль"
+            />
+            <button onClick={() => {store.login(email, password)}}>Войти</button>
+            <button onClick={() => {store.registration(email, password)}}>Зарегистрироваться</button>
+        </div>
+    )
+}
+
+export default LoginForm
